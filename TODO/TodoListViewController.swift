084cec0de64f09cc0b08dbfa7f117468533e52b0
Seankikix <Cseangodlike@gmail.com>
2019-02-07 21:29:32 +0800

    添加项目能完成

--- TODO/TodoListViewController.swift
+++ TODO/TodoListViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-    let itemArray = ["购买水杯","吃药","修改密码"]
+    var itemArray = ["购买水杯","吃药","修改密码"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -34,5 +34,25 @@ class TodoListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "添加一个新的ToDo项目", message:"", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "添加项目", style: .default, handler: {(action) in
+            //用户单击添加项目后要执行的代码
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        })
+        
+        alert.addTextField{ (alertTextField) in
+            alertTextField.placeholder = "创建一个新项目"
+
+            textField = alertTextField
+          }
+
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
